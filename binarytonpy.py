5d76bcc1bb11df53cc6fd59f2bb9c93841b97c25
VictorZoo <44965138+VictorZoo@users.noreply.github.com>
2019-09-06 15:35:37 +0800

    Update binarytonpy.py

--- binarytonpy.py
+++ binarytonpy.py
@@ -2,20 +2,20 @@
 import caffe
 import numpy as np
 
-MEAN_PROTO_PATH = '/media/hunhpc/bak/FPdataset/build_lmdb/mynet_mean.binaryproto'               
-# 待转换的pb格式图像均值文件路径
-MEAN_NPY_PATH = '/media/hunhpc/bak/FPdataset/build_lmdb/mynet_mean.npy'                        
- # 转换后的numpy格式图像均值文件路径
+MEAN_PROTO_PATH = './build_lmdb/mynet_mean.binaryproto'               
+
+MEAN_NPY_PATH = './build_lmdb/mynet_mean.npy'                        
+
 
 blob = caffe.proto.caffe_pb2.BlobProto()           
-# 创建protobuf blob
+
 data = open(MEAN_PROTO_PATH, 'rb' ).read()         
-# 读入mean.binaryproto文件内容
+
 blob.ParseFromString(data)                         
-# 解析文件内容到blob
+
 
 array = np.array(caffe.io.blobproto_to_array(blob))
-# 将blob中的均值转换成numpy格式，array的shape （mean_number，channel, hight, width）
+
 mean_npy = array[0]                               
- # 一个array中可以有多组均值存在，故需要通过下标选择其中一组均值
+
 np.save(MEAN_NPY_PATH ,mean_npy)
