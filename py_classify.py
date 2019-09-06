62525d040f8726efd071ecc925101fe97fd1d3a5
VictorZoo <44965138+VictorZoo@users.noreply.github.com>
2019-09-06 15:34:45 +0800

    Update py_classify.py

--- py_classify.py
+++ py_classify.py
@@ -1,18 +1,18 @@
 #coding=utf-8
-#加载必要的库
+
 import numpy as np
 
 import sys,os
 
-#设置当前目录
-caffe_root = '/home/hunhpc/caffe/' 
+
+caffe_root = '/your_caffe_dir/caffe/' 
 sys.path.insert(0, caffe_root + 'python')
 import caffe
 os.chdir(caffe_root)
 
 net_file=caffe_root + 'build/examples/cpp_classification/mynet_deploy.prototxt'
 caffe_model=caffe_root + 'build/examples/cpp_classification/caffenet_train_iter_100000.caffemodel'
-mean_file=caffe_root + '/media/hunhpc/bak/FPdataset/build_lmdb/mynet_mean.npy'
+mean_file=caffe_root + './mynet_mean.npy'
 
 net = caffe.Net(net_file,caffe_model,caffe.TEST)
 transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
@@ -21,7 +21,7 @@ transformer.set_mean('data', np.load(mean_file).mean(1).mean(1))
 transformer.set_raw_scale('data', 255) 
 transformer.set_channel_swap('data', (2,1,0))
 
-im=caffe.io.load_image('/media/hunhpc/bak/FPdataset/val/test_A/e402420f-3f7b-11e7-b208-e09467e031f1.png')
+im=caffe.io.load_image('./val/test_A/e402420f-3f7b-11e7-b208-e09467e031f1.png')
 net.blobs['data'].data[...] = transformer.preprocess('data',im)
 out = net.forward()
 
